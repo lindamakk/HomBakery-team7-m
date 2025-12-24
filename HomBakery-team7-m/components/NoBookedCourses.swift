960cf2720e3722b273115a106c68a5c712aa4136
Nouran-salah-ali <nouransalahali@gmail.com>
2025-12-24 16:13:44 +0300

    NoBookedCourses. Done

--- HomBakery-team7-m/components/NoBookedCourses.swift
+++ HomBakery-team7-m/components/NoBookedCourses.swift
@@ -4,4 +4,23 @@
 //
 //  Created by Linda on 23/12/2025.
 //
+import SwiftUI
+struct NoBookedCourses: View {
+    var body: some View {
+        VStack(spacing: 15){
+            Image("rollingPin")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 213, height: 43)
 
+            Text("You don’t have any booked courses")
+                .font(.system(size: 14, weight: .medium))
+                .foregroundColor(Color("grey"))
+        }//v
+        .frame(width: .infinity)
+    }//view
+}//body
+#Preview {
+    
+    NoBookedCourses()
+}
