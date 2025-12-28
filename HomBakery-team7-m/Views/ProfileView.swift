4f00ce8d72649fe922d1c416d7ed0c166a56347b
Dina-Alswailem <dina1.4075@gmail.com>
2025-12-28 12:02:13 +0300

    proflie done

--- HomBakery-team7-m/Views/ProfileView.swift
+++ HomBakery-team7-m/Views/ProfileView.swift
@@ -4,23 +4,45 @@
 //
 //  Created by Linda on 23/12/2025.
 //
-
 import SwiftUI
 import SwiftData
 
 struct ProfileView: View {
-
-    
     var body: some View {
-        ZStack {
-            Color("AppBackground")
-                            .ignoresSafeArea()
-            ProfileCard().padding()
+        NavigationStack {
+            ZStack {
+                Color("AppBackground")
+                    .ignoresSafeArea()
+
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 12) {
+
+                        // Profile section
+                        ProfileCard()
+                            .padding(.top, 8)
+
+                        // Booked courses title
+                        Text("Booked courses")
+                            .font(.headline)
+                            .padding(.top, 4)
+
+                        // Empty state (centered look)
+                        NoBookedCourses()
+                            .frame(maxWidth: .infinity)
+                            .padding(.top, 24)
+
+                        Spacer(minLength: 80)
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.top, 12)
+                }
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        
-    }}
+    }
+}
 
 #Preview {
     ProfileView()
 }
-
