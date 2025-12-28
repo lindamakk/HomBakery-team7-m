94c228dda20bedf772916cd68ab81526d4970164
Nouran-salah-ali <nouransalahali@gmail.com>
2025-12-28 12:03:27 +0300

    HomeView Done

--- HomBakery-team7-m/Views/HomeView.swift
+++ HomBakery-team7-m/Views/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftData
 
 struct HomeView: View {
 
-    
+    var isExist: Bool = false
     var body: some View {
         ZStack {
             Color("AppBackground")
@@ -18,21 +18,41 @@ struct HomeView: View {
             
             NavigationStack {
                 
-                SearchTextfield(text: .constant(""))
+                VStack(alignment: .leading){
+                    SearchTextfield(text: .constant(""))
                     .padding()
-                
-                Text("Upcoming")
-                ScrollView {
-                    LazyVStack(spacing: 8) {
-                        ForEach(courses) { course in
-                            CourseCard(course: course)
-                        }
-                    }
-                    .padding()
+                    Text("Upcoming")
+                        .padding()
+                    .font(.system(size: 24, weight: .semibold));
                 }
-                .navigationTitle("Home Bakery")
-                .navigationBarTitleDisplayMode(.inline)
-            }
+                
+
+                    
+                    if isExist{
+                        EventCard(month: "Jan", day: "15", title: "Chocolate Cake", location: "123 Baker Street", time: "18:00", onTap: {})
+                    }
+                    else{
+                        NoBookedCourses()
+                    }
+                    
+                VStack(alignment: .leading){
+                    Text("Popular courses")
+                        .padding()
+                    .font(.system(size: 24, weight: .semibold));
+                    ScrollView {
+                        LazyVStack(spacing: 8) {
+                            ForEach(courses) { course in
+                                CourseCard(course: course)
+                            }
+                        }
+                        .padding()
+                    }
+                    .navigationTitle("Home Bakery")
+                    .navigationBarTitleDisplayMode(.inline)
+                    
+                    
+                }//v
+            }//nav
     }
     }}
 
