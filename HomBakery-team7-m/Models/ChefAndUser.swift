69c4f96e9f5c7e698f8199f51d8ab9c3c3879b3b
Nouran-salah-ali <nouransalahali@gmail.com>
2026-01-01 09:58:58 +0300

    user fetch done

--- HomBakery-team7-m/Models/ChefAndUser.swift
+++ HomBakery-team7-m/Models/ChefAndUser.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-//struct UserAndChefResponse: Codable {
-//    let records: [UserAndChef]
-//}
+struct UserAndChefResponse: Codable {
+    let records: [UserAndChef]
+}
 // MARK: - UserAndChef
 struct UserAndChef: Codable {
     let id: String
